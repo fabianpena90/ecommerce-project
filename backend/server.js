d86d88989697ff70c051d7ab2793e974190b544a
Fabian Pena <camilo.fabis@gmail.com>
2020-11-27 15:20:37 -0500

    update profile, login route logout route added

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
@@ -13,12 +14,15 @@ connectDB()
 
 const app = express();
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('Api is running')
 })
 
 // Routes
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 // Middlewares
 app.use(notFound)
