3b5a9dc322545485a9c7f7a69afc8c39ca97ca39
Fabian Pena <camilo.fabis@gmail.com>
2020-11-27 01:22:16 -0500

    created actions, reducers

---
+++ frontend/src/reducers/cartReducers.js
@@ -0,0 +1,32 @@
+import { 
+    CART_ADD_ITEM,
+    CART_REMOVE_ITEM 
+} from '../constants/cartConstants'
+
+export const cartReducer = (state = { cartItems: [] }, action) => {
+    switch (action.type) {
+        case CART_ADD_ITEM:
+            const item = action.payload
+
+            const existItem = state.cartItems.find(val => val.product === item.product)
+
+            if(existItem) {
+                return {
+                    ...state,
+                    cartItems: state.cartItems.map(val => val.product === existItem.product ? item : val)
+                }
+            } else {
+                return {
+                    ...state,
+                    cartItems: [...state.cartItems, item]
+                }
+            }
+        case CART_REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(val => val.product !== action.payload)
+            }
+        default: 
+        return state
+    }
+}
